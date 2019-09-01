a4989fe3da83328a9c89abc049ddf3c2cc82a1c2
Yunen Ramos R <yunencr@outlook.com>
2019-09-01 12:56:58 -0600

    Se explican los tipos de datos para enteros, enteros con precisión y texto. También como obtener el primer y último caracter de un String.

--- 02-tipos-numero-string.dart
+++ 02-tipos-numero-string.dart
@@ -1,16 +1,24 @@
-void main() {
-  
-//   	Números
+void main(){
+  /// Variable para almacenar números enteros.
   int empleados = 10;
+  
+  /// Variable para almacenar números con precisión.
   double pi = 3.141592;
+  
+  /// Declaración dinámica
   var numero = 1.0;
   
-  print( '$empleados - $pi - $numero' );
+  // Se imprimen en consola.
+  print('$empleados - $pi - $numero');
   
-//   String - Cadenas de caracteres
+  /// Variable para almacenar texto.
   String nombre = 'Tony';
-  print(nombre);
-  print(nombre[0]);
-  print(nombre[ nombre.length - 1 ]);
   
+  /// Variable para almacenar texto con escape.
+  String nombreEscape = 'To\'ny'; // Para lograrlo se usa el: \
+  
+  // se imprime en consola.
+  print('$nombre - $nombreEscape');
+  print(nombre[0]); // Se obtiene el primer caracter del String.
+  print(nombre[nombre.length - 1]); // Se obtiene el último caracter del String.
 }
