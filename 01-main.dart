9d88d1293d348629d52d6b683232ad811c32c26b
Yunen Ramos R <yunencr@outlook.com>
2019-09-01 11:42:06 -0600

    Se explica los tipos de comentarios, declaración de variables por var o tipo de dato, también se indica como mostrar una variable al momento de imprimir en consola.

--- 01-main.dart
+++ 01-main.dart
@@ -1,15 +1,17 @@
-void main() {
-  
-  /*
+/// Función principal para indicarle a Dart.
+/// El void igual que en otros lenguajes indica que la función no retorna nada.
+void main(){
   print('Hola Mundo');
-  asdasd
-  asd
-  a
-  sdasd
-  */
+  // Una manera de hacer comentarios. Estos comentarios no se agregan en la documentación.
+  /* 
+   * Este es un comentario multilínea. Tampoco se agrega a la documentación.
+   *  */
   
-  String nombre;
-
-  print('Hola $nombre');
+  /// Variable para almacenar un nombre
+  var nombre = 'Yunen'; // Dinámicamente Dart asume el tipo según el valor brindado.
+  String nombreDos = 'Yunen'; // Es mejor indicarle el tipo, con el fin de poder acceder a la ayuda de los métodos disponibles.
   
+  print('Hola Yunen - Quemado'); // Texto quemado
+  print('Hola ' + nombre + ' - Por concatenación con el símbolo +'); // Concatenación por con el símbolo de más.
+  print('Hola $nombreDos - Por interpolación'); // Se interpola en el string.
 }
