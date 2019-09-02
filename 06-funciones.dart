be01a409be394b56f14a191638b340397d393081
Yunen Ramos R <yunencr@outlook.com>
2019-09-01 19:29:44 -0600

    Se explican las funciones void y con retorno. Con parámetros dynamic, definidos por tipo y definidos por nombre. También se define una función flecha.

--- 06-funciones.dart
+++ 06-funciones.dart
@@ -1,15 +1,70 @@
-void main() {
+void main(){
+  /// Se llama a la función saludar.
+  saludar();
+  saludarVoid();
   
-  String mensaje = saludar2( nombre: 'Fernando', texto: 'Hola,'  );
+  // Se almacena el resultado de una función en un String.
+  String mensaje = saludarString();
   
+  // Se imprime el mensaje en consola.
   print(mensaje);
   
+  // Se le asigna el valor de la función que tiene parámetros a la variable.
+  mensaje = saludarParametros('Hola,', 'Yunen');
+  
+  // Se imprime el mensaje en consola.
+  print(mensaje);
+  
+  // Se le asigna el valor de la función que tiene parámetros a la variable.
+  mensaje = saludarParametrosString('Hola por segunda vez,', 'Tony');
+  
+  // Se imprime el mensaje en consola.
+  print(mensaje);
+  
+  // Se le asigna el valor de la función que tiene parámetros a la variable. Se debe indicar el parámetro que se está pasando
+  mensaje = saludarParametrosConNombre(texto:'Hola por tercera vez,',nombre:'Banner');
+  
+  // Se imprime el mensaje en consola.
+  print(mensaje);
+  
+  // Se le asigna el valor de la función flecha.
+  mensaje = saludarFlecha(texto:'Hola por cuarta vez,',nombre:'Strange');
+  
+  // Se imprime el mensaje en consola.
+  print(mensaje);
 }
 
+/// Se declara una función dinámica
+saludar(){
+  print('Hola');
+}
 
-String saludar({ String texto, String nombre }) {
-//   print('Hola');
+/// Se declara una función void, es el tipo a especificar cuando no retornada nada.
+void saludarVoid(){
+  print('Hola void');
+}
+
+/// Se declara una función que retorna un String.
+String saludarString(){
+  /// La palabra reservada "return" se emplea para retornar un valor.
+  return 'Hola';
+}
+
+/// Se retorna un String con la información de [texto] y de [nombre].
+/// Función con parámetros dinámicos.
+String saludarParametros(texto, nombre){
   return '$texto $nombre';
 }
 
-String saludar2({ String texto, String nombre }) => '$texto $nombre';
+/// Se le define el tipo de variable a los parámetros recibidos.
+String saludarParametrosString(String texto, String nombre){
+  return '$texto $nombre';
+}
+
+/// Se encierran los parámetros en llave, para que los tengan que definir cuando los vayan a pasar.
+String saludarParametrosConNombre({String texto, String nombre}){
+  return '$texto $nombre';
+}
+
+/// Se define una flecha.
+String saludarFlecha({String texto, String nombre}) => '$texto $nombre';
