a6927c73b12693e1b9076f3b9faa5958170b08ee
Yunen Ramos R <yunencr@outlook.com>
2019-09-01 18:28:55 -0600

    Se explica sobre mapas dinámicos y con llaves y valores definidos. También como obtener posiciones del mapa y como agregarle valores.

--- 05-map.dart
+++ 05-map.dart
@@ -1,31 +1,35 @@
-
-void main() {
-  
+void main(){
   String propiedad = 'soltero';
   
-  
+  /// Los map son un conjunto de pares y valores.
+  /// Se le puede indicar el tipo de la clave y del valor, por defecto si no se indica se asume dinámico.
   Map<String, dynamic> persona = {
-    'nombre' : 'Carlos',
-    'edad'   : 32,
+    'nombre': 'Carlos',
+    'edad': 32,
     'soltero': true
   };
   
-  print( persona['nombre'] );
-  print( persona['edad'] );
-  print( persona[ propiedad ] );
+  // Se imprime en consola el nombre, edad.
+  print(persona['nombre']);
+  print(persona['edad']);
   
+  // Se imprime por medio del contenido de una variable
+  print(persona[propiedad]);
   
+  /// Se crea un mapa que posee como clave valores enteros
   Map<int, String> personas = {
     1: 'Tony',
     2: 'Peter',
     9: 'Strange'
   };
   
-  personas.addAll( { 4: 'Banner'  } );
+  /// Se le agrega una nueva persona.
+  personas.addAll({4: 'Banner'});
   
-  print( personas );
-  print( personas[2] );
+  // Se imprime el mapa de personas.
+  print(personas);
   
-  
+  // Se imprime el nombre de Peter.
+  // Se usa el número 2, porque es la clave que almace el valor de Peter.
+  print(personas[2]);
 }
-
