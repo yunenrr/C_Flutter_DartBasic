f4fdb3545c114ebac5658bd28230a5aa5d96f73d
Yunen Ramos R <yunencr@outlook.com>
2019-09-01 21:48:53 -0600

    Se menciona como se crean los set y los get. También como crear los throw que sirven para gestionar errores.

--- 09-getters-setters.dart
+++ 09-getters-setters.dart
@@ -1,37 +1,33 @@
-void main() {
+
+void main(){
+  final Cuadrado cuadrado = new Cuadrado();
   
-  final cuadrado = new Cuadrado();
-  
+  // Se asigna un valor mediante el set.
   cuadrado.lado = 10;
   
-  
-  print( cuadrado );
-  print( 'área: ${ cuadrado.area }' );
-  
+  // Se imprime en consola.
+  print(cuadrado);
+  print('${cuadrado.area}');
 }
 
-
-
-class Cuadrado {
-  
+class Cuadrado{
+  // El guión bajo indica que son propiedade privadas.
   double _lado;
-//   double _area;
   
-  
-  set lado( double valor ) {
-    
-    if ( valor <= 0 ) {
-      throw('El lado no puede ser menor o igual a 0');
+  /// Se crea un set para la propiedad lado
+  set lado(double valor){
+    // Se valida que el valor debe ser mayor a cro.
+    if(valor <= 0){
+      // Se retorna un error.
+      throw('El valor no puede ser menor o igual a 0.');
     }
     
+    // Se asigna el valor a la propiedad.
     _lado = valor;
-    
   }
   
-  
+  /// Va sin paréntesis.
   double get area => _lado * _lado;
   
-  
-  toString() => 'Lado: $_lado';
-  
+  String toString() => 'Lado: $_lado - Área: $area';
 }
