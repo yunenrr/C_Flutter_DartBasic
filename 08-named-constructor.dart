ab90b560dadb0d6ccdef56c64ad571d7d258bc15
Yunen Ramos R <yunencr@outlook.com>
2019-09-01 21:28:53 -0600

    Se crea un método con constructor con nombe.

--- 08-named-constructor.dart
+++ 08-named-constructor.dart
@@ -1,38 +1,42 @@
+// Se importa un paquete para poder convertir el String.
 import 'dart:convert';
 
-void main() {
+void main(){
+  final Heroe wolverine = new Heroe(nombre: 'Logan', poder: 'Regeneración');
   
-//   final wolverine = new Heroe('Logan', 'Regeneración');
-  final rawJson    = '{ "nombre": "Logan", "poder":"Regeneración" }';
-  Map parsedJson = json.decode( rawJson );
+  // Se crea un JSON String.
+  // Debe ser un JSON valido.
+  final String rawJSON = '{"nombre":"Thanos","poder":"Morado"}';
+  final Map parseJSON = json.decode(rawJSON);
   
-//   print( parsedJson );
+  // Se imprime el parseJSON.
+  print(parseJSON);
   
-  final wolverine = new Heroe.fromJson( parsedJson );
-  
+  // Se imprimen en consola.
   print(wolverine.nombre);
   print(wolverine.poder);
   
+  // Se crea una variable para almacenar el valor del Map.
+  final Heroe thanos = new Heroe.fromJson(parseJSON);
+  
+  // Se imprimen en consola.
+  print(thanos.nombre);
+  print(thanos.poder);
 }
 
+// La función no está dentro de una clase / Los métodos están dentro de una clase.
 
-
-
-
-class Heroe {
-  
+/// Se define una clase que posee propiedades.
+class Heroe{
   String nombre;
   String poder;
   
-  Heroe( this.nombre, this.poder );
-  
-	Heroe.fromJson( Map parsedJson ) {
-    nombre = parsedJson['nombre'];
-    poder  = parsedJson['poder'];
-    
+  /// Constructor sin parámetros.
+  Heroe({this.nombre = 'Sin nombre', this.poder});
+
+  /// Constructor con nombre, el punto se utiliza para indicarle el nombre.
+  Heroe.fromJson(Map parsedJSON){
+    nombre = parsedJSON['nombre'];
+    poder = parsedJSON['poder'];
   }
-  
-  
 }
-
-
