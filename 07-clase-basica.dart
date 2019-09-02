52768e08756fefc77ce4dc5503dc74951ea56ee3
Yunen Ramos R <yunencr@outlook.com>
2019-09-01 20:58:01 -0600

    Se explica como crear clases, agregarle parámetros, método constructor con argumentos con nombre y se sobreescribe el método toString.

--- 07-clase-basica.dart
+++ 07-clase-basica.dart
@@ -1,28 +1,38 @@
-void main() {
+void main(){
+  // Se crea una instancia de la clase Heroe.
+  // La palabra reservada new es opcional. Se coloca por tradición cuando se hace una asignación a una variable.
+  /// Se emplea la palabra reservada final para indicarle que su valor nunca va a cambiar. Las propiedades se pueden cambiar, pero no el valor.
+  final wolverine = new Heroe(nombre: 'Logan', poder: 'Regeneración');
   
-  final wolverine = new Heroe( 
-    poder: 'Regeneración',
-    nombre: 'Logan'
-  );
-    
-  print( wolverine );
+  // Se imprime en consola la instancia.
+  print(wolverine); // Si no se modifica el toString entonces aparece: Instance of 'Heroe'
   
+  // Se imprime en consola las propiedades.
+  print(wolverine.nombre);
+  print(wolverine.poder);
+  
+  final unknow = new Heroe(poder: 'Inmortal');
+  
+  // Se imprime en consola la instancia.
+  print(unknow);
 }
 
-class Heroe {
-  
+// La función no está dentro de una clase / Los métodos están dentro de una clase.
+
+/// Se define una clase que posee propiedades.
+class Heroe{
   String nombre;
   String poder;
   
-//   Heroe({ String nombre = 'Sin Nombre', String poder }) {
-//     this.nombre = nombre;
-//     this.poder  = poder; 
-//   }
+  /// Constructor
+  /// Se le indica que el nombre es opcional
+  /// Son argumentos con nombre.
+  Heroe({String nombre = 'Sin nombre', String poder}){
+    this.nombre = nombre;
+    this.poder = poder;
+  }
   
-  Heroe({ this.nombre, this.poder });
-  
-  String toString() => 'nombre: $nombre - poder: $poder';
-  
+  /// Se sobreescibre método toString.
+  /// Cuando se ocupa un primer una variable con caracter se implementa el: ${}.
+  String toString() => 'Nombre: ${this.nombre} - Poder: ${this.poder}';
 }
-
-
