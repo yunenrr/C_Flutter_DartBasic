f8bae690dd00b1fa8090d3d475c83e3595ddf856
Yunen Ramos R <yunencr@outlook.com>
2019-09-08 11:35:14 -0600

    Ejemplo de uso de los Futures.

--- 13-futures.dart
+++ 13-futures.dart
@@ -1,24 +1,22 @@
-
-void main() {
+void main(){
+  // Se muestra un mensaje en consola
+  print('Estamos a punto de pedir datos');
   
-	print('Estamos a punto de pedir datos');
-  
-  httpGet('https://api.nasa.com/aliens').then( (data) {
-    
-    print( data );
-    
+  // Se llama al método que retorna el Future.
+  // El ".then" es para ejecutar el código con la respuesta del Future.
+  httpGet('https://api.nasa.com/aliens').then((data){
+    // Se imprime el mensaje del Future.
+    print(data);
   });
   
-  
-  print('Ultima línea');
+  // Se muestra un mensaje en consola
+  print('última línea');
 }
 
-
-
-Future<String> httpGet(String url) {
-  
-  return Future.delayed( new Duration( seconds: 4 ), () {
-    return 'Hola Mundo';
+/// La palabra reservada "Future" le indica a Dart que este método retorna un Future.
+Future<String> httpGet(String url){
+  /// La función delayed ejecuta una tarea cuando pasa cierta cantidad de tiempo.
+  return Future.delayed(new Duration(seconds: 4), () {
+    return 'Hola mundo';
   });
-  
 }
