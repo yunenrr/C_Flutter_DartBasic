1e686667174417cffca64e1721b1e825de061fdb
Yunen Ramos R <yunencr@outlook.com>
2019-09-08 11:43:03 -0600

    Ejemplo de uso del async y await.

--- 14-async-await.dart
+++ 14-async-await.dart
@@ -1,24 +1,24 @@
-
-void main() async {
+/// async permite convertir una tarea en asíncrona
+/// Los constructores no pueden ser asíncronos.
+/// await permite esperar la respuesta de un Future, para usarlo es obligatorio utilizarlo en un método async.
+void main() async{
+  // Se muestra un mensaje en consola
+  print('Estamos a punto de pedir datos');
   
-	print('Estamos a punto de pedir datos');
-  
+  // Se llama al método que retorna el Future.
   String data = await httpGet('https://api.nasa.com/aliens');
   
-  print( data );
+  // Se muestra el resultado del Future en consola.
+  print(data);
   
-  print('Ultima línea');
+  // Se muestra un mensaje en consola
+  print('última línea');
 }
 
-
-
-Future<String> httpGet(String url) {
-  
-  return Future.delayed( new Duration( seconds: 4 ), () {
-    return 'Hola Mundo';
+/// La palabra reservada "Future" le indica a Dart que este método retorna un Future.
+Future<String> httpGet(String url){
+  /// La función delayed ejecuta una tarea cuando pasa cierta cantidad de tiempo.
+  return Future.delayed(new Duration(seconds: 4), () {
+    return 'Hola mundo';
   });
-  
 }
-
-
-
