d9f3810242fbfb39f0031a7ed1e3f80d334aba58
Yunen Ramos R <yunencr@outlook.com>
2019-09-08 09:31:58 -0600

    Se menciona qué son clases abstractas y dos ejemplos.

--- 10-clases-abstractas.dart
+++ 10-clases-abstractas.dart
@@ -1,38 +1,29 @@
-void main() {
-  
+
+void main(){
   final perro = new Perro();
   perro.emitirSonido();
   
   final gato = new Gato();
   gato.emitirSonido();
-  
 }
 
-
-abstract class Animal {
-  
+/// abstract le indica a Dart que es una clase asbtracta.
+/// Una clase abstracta obliga a otras clases implementar las propiedades y métodos.
+abstract class Animal{
   int patas;
-  
   void emitirSonido();
-  
 }
 
+/// La palabra clave "implements" sirve para implementar clases abstractas.
 class Perro implements Animal{
-  
   int patas;
   int colas;
   
-  void emitirSonido() => print('GUAUUUUU!!');
-  
+  void emitirSonido() => print('GUAUUU!');
 }
 
-class Gato implements Animal {
-  
+class Gato implements Animal{
   int patas;
   
-  void emitirSonido() => print('MIAUUUU!!');
-  
+  void emitirSonido() => print('MIAUUU!');
 }
-
-
-
